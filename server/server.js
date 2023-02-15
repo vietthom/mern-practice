5e8cdbd5855b2eff7068633bc66e448da6d65bfa
vietthom <thomle0418@gmail.com>
2023-02-15 14:05:35 -0700

    establish record routes

--- server/server.js
+++ server/server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 6000;
 app.use(cors());
 app.use(express.json());
-app.use(requrie("./routes/record"));
-// get drive connection
+app.use(require("./routes/record"));
+// get driver connection
 const dbo = require("./db/conn");
-
-app.listen(port, () =>{
-  dbo.connectToServer(function(err) {
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
     if (err) console.error(err);
+ 
   });
-  console.log(`Server is running on port: $[port]`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
